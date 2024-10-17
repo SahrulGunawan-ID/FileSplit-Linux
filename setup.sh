25428cbfe459dc6ca76dd6b67eaf5716c0a3f157
Sahrul Gunawan Cyber <developer.limited.id@gmail.com>
2024-10-18 00:31:13 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+###############################
+# Author : Sahrul Gunawan Cyber
+# Channel YouTube : https://youtube.com/@bangtutorialofficial1152
+# Telegram : https://t.me/sahrulgunawan_id
+# Official Github : https://github.com/SahrulGunawan-ID
+###############################
+clear
+
+apt update && apt upgrade -y
+apt install tree hr -y
+
+sh split-file.sh
